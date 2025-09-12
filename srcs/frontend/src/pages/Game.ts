0a65143a336fa31f4b610e7332ec0401742ec0e7
pauldahacker <paul.demasca@gmail.com>
2025-09-12 13:03:48 +0200

    added tournament matchmaking. still not fully implemented but pretty clean start

--- srcs/frontend/src/pages/Game.ts
+++ srcs/frontend/src/pages/Game.ts
@@ -11,13 +11,13 @@ export function renderGame(root: HTMLElement, options: RenderGameOptions = {}) {
 
   const container = document.createElement("div");
   container.className =
-    "flex flex-col justify-between items-center h-screen pt-[2vh] pb-[2vh] min-h-[400px] min-w-[600px] relative";
+    "flex flex-col justify-between items-center h-screen pt-[2vh] pb-[2vh] min-h-[400px] min-w-[600px] relative mx-auto my-auto";
 
   container.innerHTML = `
     <h1 class="font-honk text-[5vh] animate-wobble">Pong</h1>
 
     <div class="flex items-center justify-center gap-[2vw]">
-      <div class="font-honk text-[5vh] truncate">${player1}</div>
+      <div class="font-honk text-[4vh] text-center truncate w-[8ch] shrink-0">${player1}</div>
       <div id="game-container" 
            class="relative h-[80vh] aspect-[3/2] 
                   max-w-[calc(100vw-100px)] max-h-[calc(100vh-100px)] 
@@ -25,7 +25,7 @@ export function renderGame(root: HTMLElement, options: RenderGameOptions = {}) {
         <canvas id="game-canvas" 
                 class="bg-cyan-950 w-full h-full border border-gray-500 rounded"></canvas>
       </div>
-      <div class="font-honk text-[5vh] truncate">${player2}</div>
+      <div class="font-honk text-[4vh] text-center truncate w-[8ch] shrink-0">${player2}</div>
     </div>
 
     <a id="back-home" href="#/" 
@@ -43,27 +43,38 @@ export function renderGame(root: HTMLElement, options: RenderGameOptions = {}) {
   const gameContainer = container.querySelector<HTMLDivElement>("#game-container")!;
   const backHomeButton = container.querySelector<HTMLAnchorElement>("#back-home")!;
 
-  const stopGame = startPong(canvas, (winner: number) => {
-    const overlay = document.createElement("div");
-    overlay.className = "absolute inset-0 flex justify-center items-center";
-    overlay.innerHTML = `
-      <div class="relative inline-block text-center">
-        <h2 class="absolute inset-0 text-[10vh] font-bit text-lime-500 text-center animate-zoomIn
-               [text-shadow:-2px_-2px_0_#fff,2px_-2px_0_#fff,-2px_2px_0_#fff,2px_2px_0_#fff]">
-          ${winner === 1 ? player1 : player2} Wins!
-        </h2>
-        <h2 class="relative text-[10vh] font-bit text-lime-500 text-center animate-zoomIn">
-          ${winner === 1 ? player1 : player2} Wins!
-        </h2>
-      </div>
-    `;
-    gameContainer.appendChild(overlay);
-    
-    if (onGameOver) {
-      setTimeout(() => {
-        onGameOver(winner);
-      }, 2000); // delay 4 seconds
-    }
+  let stopGame: () => void;
+
+  requestAnimationFrame(() => {
+    stopGame = startPong(canvas, (winner: number) => {
+      const overlay = document.createElement("div");
+      overlay.className = "absolute inset-0 flex justify-center items-center";
+      overlay.innerHTML = `
+        <div class="relative inline-block text-center">
+          <h2 class="absolute inset-0 text-[10vh] font-bit text-lime-500 text-center animate-zoomIn
+                 [text-shadow:-1px_-1px_0_#fff,1px_-1px_0_#fff,-1px_1px_0_#fff,1px_1px_0_#fff]">
+            ${winner === 1 ? player1 : player2} Wins!
+          </h2>
+          <h2 class="relative text-[10vh] font-bit text-lime-500 text-center animate-zoomIn">
+            ${winner === 1 ? player1 : player2} Wins!
+          </h2>
+        </div>
+      `;
+      gameContainer.appendChild(overlay);
+
+      if (onGameOver) {
+        setTimeout(() => {
+          onGameOver(winner);
+        }, 2000);
+      }
+    });
   });
-  
+
+  backHomeButton.addEventListener("click", () => {
+    if (stopGame) stopGame(); // cleanup if already started
+  });
+
+  return () => {
+    if (stopGame) stopGame();
+  };
 }
