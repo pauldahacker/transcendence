c1887ce9fb7dd335a824353ea9abab4adeb09205
pauldahacker <paul.demasca@gmail.com>
2025-09-15 19:42:24 +0200

    added match list before starting a tournament, the tournament does not do the final game (between the 2 winners) for 4 players yet. UI still not perfect but shouldnt care for now

--- srcs/frontend/src/pages/Home.ts
+++ srcs/frontend/src/pages/Home.ts
@@ -6,13 +6,13 @@ export function renderHome(root: HTMLElement) {
   container.innerHTML = `
     <h1 class="font-honk text-[25vh] animate-wobble">Pong</h1>
     <a href="#/game" 
-       class="flex items-center justify-center w-[25vw] h-[8vh] rounded-full
+       class="flex items-center justify-center w-[25vw] h-[8vh] rounded-full min-w-[300px]
               border-2 border-gray-100 text-gray-100 font-bit text-[5vh]
               transition-colors duration-300 hover:bg-gray-100 hover:text-cyan-900">
        1v1
     </a>
     <a href="#/tournament" 
-       class="flex items-center justify-center w-[25vw] h-[8vh] rounded-full
+       class="flex items-center justify-center w-[25vw] h-[8vh] rounded-full min-w-[300px]
               border-2 border-gray-100 text-gray-100 font-bit text-[5vh]
               transition-colors duration-300 hover:bg-gray-100 hover:text-cyan-900">
        Tournament
