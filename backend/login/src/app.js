a683e3e78ed3aed5dd255649990030af20b9823e
Fran Marín <franmp97@outlook.com>
2025-10-01 17:44:07 +0200

    project structure changed, frontend deployment updated

---
+++ backend/login/src/app.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.get('/', function (request, reply) {
+  reply.send({ status: 'login service' })
+})
+
+fastify.listen({ host: "0.0.0.0", port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
