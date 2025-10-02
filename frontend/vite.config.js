bb8cb73ed97f3b39820e2d5f448c8c8046bc047d
Fran Marín <franmp97@outlook.com>
2025-10-02 13:53:08 +0200

    api gateway redirecting to all apps, exposed internal ports as env variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 
+const PORT = process.env.FRONTEND_PORT || 3000;
+
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: PORT,
+    allowedHosts: ['frontend']
+  }
 });
