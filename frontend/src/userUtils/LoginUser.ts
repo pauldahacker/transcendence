800d286c3c786554761fa886871944e58d11b187
Ivan_hua <ivan@ivanhua.localdomain>
2025-10-09 18:32:11 +0200

    Login Register and Logout works very basic, to add: from profile frontend access and modify user data, check if the token is valid,  on the rest of the frontend get the name and put it instead of player1, in the login or register add the error messages to the frontend maybe

--- frontend/src/userUtils/LoginUser.ts
+++ frontend/src/userUtils/LoginUser.ts
@@ -24,22 +24,6 @@ export async function login(username: string, password: string): Promise<string>
   if (!data.token) throw new Error("El servidor no devolvió ningún token.");
 
   localStorage.setItem("auth_token", data.token);
+  window.location.hash = "#/profile";
   return data.token;
-}
-
-export function getUsernameFromToken(token: string): string | null {
-  try {
-    const base64Url = token.split(".")[1]; // parte del payload
-    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split("")
-        .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
-        .join("")
-    );
-    const payload = JSON.parse(jsonPayload);
-    return payload.username || null;
-  } catch {
-    return null;
-  }
 }
