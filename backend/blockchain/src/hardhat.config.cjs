0f0c01c42b3e72198d03109c60f90d629c9f2510
rzhdanov <rkz.myway@gmail.com>
2025-10-16 22:28:04 +0300

    corrected pathways and file locations for hardhat

--- backend/blockchain/src/hardhat.config.cjs
+++ backend/blockchain/src/hardhat.config.cjs
@@ -6,7 +6,7 @@
 /*   By: rzhdanov <rzhdanov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 16:31:28 by rzhdanov          #+#    #+#             */
-/*   Updated: 2025/10/13 23:09:02 by rzhdanov         ###   ########.fr       */
+/*   Updated: 2025/10/16 22:18:23 by rzhdanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 require('@nomicfoundation/hardhat-toolbox');
 
 module.exports = {
-  solidity: {
-    version: '0.8.24',
-    settings: { optimizer: { enabled: true, runs: 200 } }
+  solidity: '0.8.24',
+  paths: {
+    sources: './contracts',
+    // write outpust to parent for /abi endpoint to work properly
+    artifacts: './artifacts',
+    cache: './cache',
+    tests: './tests' // unused so far, we use node test.js at the moment
   }
-  // No netwroks  now 
 };
