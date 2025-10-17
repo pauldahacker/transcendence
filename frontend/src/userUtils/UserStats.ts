602417a5233ca1aa812684b7867f1001112f901c
Ivan_hua <ivan@ivanhua.localdomain>
2025-10-17 13:59:55 +0200

    userstatsnotfound

--- frontend/src/userUtils/UserStats.ts
+++ frontend/src/userUtils/UserStats.ts
@@ -14,65 +14,41 @@ export type UserStats = {
 	stats: UserStats;
   };
 
-  const token = localStorage.getItem("auth_token");
+const token = localStorage.getItem("auth_token");
 
-export async function getUserId(username: string){
-   const res = await fetch("/api/users/", {
-	headers: {
+export async function getUserId(username: string) {
+  const res = await fetch("/api/users/", {
+    headers: {
       "Authorization": `Bearer ${token}`,
     },
   });
-  if (!res.ok) throw new Error(`Error ${res.status}`);
+  if (!res.ok) {
+  	const text = await res.text();
+  	console.log("Respuesta del servidor:", res.status, text);
+  	throw new Error(`Error ${res.status}`);
+}
   const users = await res.json();
   const user = users.find((u: { id: number; username: string }) => u.username === username);
-  
+  console.log(`user_id encontrado: ${user.id}`)
   return user?.id ?? null;
 }
 
-export async function getUserData(userId: number): Promise<UserData> {
-	const token = localStorage.getItem("auth_token");
-  
-	const res = await fetch(`/api/users/${userId}/`, {
-	  method: "GET",
-	  headers: {
-		"Authorization": `Bearer ${token}`,
-		"Content-Type": "application/json",
-	  },
-	});
-  
-	if (!res.ok) {
-	  let errorMsg = "";
-	  try {
-		const maybeJson = await res.json();
-		errorMsg = maybeJson?.message || JSON.stringify(maybeJson);
-	  } catch {
-		errorMsg = await res.text();
-	  }
-	  throw new Error(errorMsg || `Error ${res.status}`);
-	}
-  
-	const data = await res.json();
-  	return {
-	  username: data.username,
-	  display_name: data.display_name,
-	  avatar_url: data.avatar_url,
-	  bio: data.bio,
-	  created_at: data.created_at,
-	  friends: data.friends ?? [],
-	  stats: {
-		total_games: data.stats.total_games ?? 0,
-		wins: data.stats.wins ?? 0,
-		losses: data.stats.losses ?? 0,
-	  },
-	};
+export async function getUserData(userId: number) {
+  const res = await fetch(`/api/users/${userId}/`, {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${token}`,
+    },
+  });
+  if (!res.ok){
+	console.log(`Error al acceder a /api/users/${userId}/`)
+	throw new Error(`Error ${res.status}`);
   }
+  return await res.json();
+}
 
-  export async function getUserDataFromName(username :string){
-	try {
-		const userId = await getUserId(username);
-		const UserData = await getUserData(userId);
-		return UserData;
-	} catch (e: any){
-		throw e;
-	}
-  }
+export async function getUserDataFromName(username: string) {
+  const id = await getUserId(username);
+  if (!id) throw new Error(`Usuario "${username}" no encontrado`);
+  return await getUserData(id);
+}
