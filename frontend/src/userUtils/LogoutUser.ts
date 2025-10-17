48742767d2b1b03244c1d2ce58bf3bd40d99bb99
Ivan_hua <ivan@ivanhua.localdomain>
2025-10-17 23:58:37 +0200

    Profile page works for avatar and bio and stats, left to update stats after game, changed security policies in default.conf.template to accept image links as avatar

--- frontend/src/userUtils/LogoutUser.ts
+++ frontend/src/userUtils/LogoutUser.ts
@@ -3,3 +3,31 @@ export function logout(){
     alert("You've logged out succesfully");
     window.location.hash = "#/";
 }
+
+export async function logoutUser() {
+  const token = localStorage.getItem("auth_token");
+  if (!token) return;
+
+  try {
+    const res = await fetch("/api/users/logout", {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+      },
+    });
+
+    if (!res.ok) {
+      if (res.status === 401) {
+        console.warn("Invalid token");
+      } else {
+        console.error(`Error logout: ${res.status}`);
+      }
+    }
+    localStorage.removeItem("auth_token");
+    console.log("user logged out succesfully");
+  } catch (err) {
+    console.error("Error logout:", err);
+    localStorage.removeItem("auth_token");
+  }
+  window.location.hash = "#/";
+}
