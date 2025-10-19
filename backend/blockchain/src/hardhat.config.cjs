4c93ce7a33cfc74b74e5d711cbfb8386f05c94c8
rzhdanov <rkz.myway@gmail.com>
2025-10-19 16:43:43 +0300

    blockchain: made deploy.cjs env guarded and added fuij network with npm script

--- backend/blockchain/src/hardhat.config.cjs
+++ backend/blockchain/src/hardhat.config.cjs
@@ -6,13 +6,15 @@
 /*   By: rzhdanov <rzhdanov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 16:31:28 by rzhdanov          #+#    #+#             */
-/*   Updated: 2025/10/16 22:18:23 by rzhdanov         ###   ########.fr       */
+/*   Updated: 2025/10/19 16:27:15 by rzhdanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 'use strict';
 
 require('@nomicfoundation/hardhat-toolbox');
+const RPC_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: '0.8.24',
@@ -22,5 +24,15 @@ module.exports = {
     artifacts: './artifacts',
     cache: './cache',
     tests: './tests' // unused so far, we use node test.js at the moment
-  }
+  },
+    networks: (() => {
+    const nets = {};
+    if (RPC_URL && PRIVATE_KEY) {
+      nets.fuji = {
+        url: RPC_URL,
+        accounts: [PRIVATE_KEY],
+      };
+    }
+    return nets;
+  })(),
 };
