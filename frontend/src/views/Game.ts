6d926ad98c1eb1113dd4b824d95b494ac02849cb
Ivan Lorenzo Cutrona <ilorenzo@cbr12s4.42barcelona.com>
2025-10-25 17:26:23 +0200

    tournaments bug fixed

--- frontend/src/views/Game.ts
+++ frontend/src/views/Game.ts
@@ -1,6 +1,6 @@
 import { startPong } from "../pong/startPong";
 import { startPong3D } from "../3d/renderStart";
-import { is3DActive } from "@/tournament/state";
+import { is3DActive, type TournamentState } from "@/tournament/state";
 import { postMatch, generateMatchId } from "@/userUtils/UserMatch";
 import { getUserIdFromToken, getUsernameFromToken, getDisplayName} from "@/userUtils";
 import { isUserLoggedIn } from "@/userUtils";
@@ -9,12 +9,12 @@ import type { GameOverState } from "@/pong/types";
 type RenderGameOptions = {
   onePlayer?: boolean;
   tournament?: boolean;
-
+  tournamentState?: TournamentState;
   player1?: string;
   player2?: string;
   aiPlayer1?: boolean;
   aiPlayer2?: boolean;
-  onGameOver?: (result: GameOverState) => void;
+  onGameOver?: (result: GameOverState, tournament?: TournamentState) => void;
 };
 
 /*
@@ -30,6 +30,7 @@ export async function renderGame(root: HTMLElement, options: RenderGameOptions =
   const {
     onePlayer = false,
     tournament = false,
+    tournamentState,
     onGameOver
   } = options;
 
@@ -103,15 +104,21 @@ export async function renderGame(root: HTMLElement, options: RenderGameOptions =
         const overlay = document.createElement("div");
         overlay.className = "absolute inset-0 flex flex-col justify-center items-center gap-6";
         
-		if (!(aiP2 && aiP1)){
-			const userScore = aiP1 && !aiP2 ? score2 : score1;
-          	const opponentScore = aiP1 && !aiP2 ? score1 : score2;
-			try {
-            	await postMatch({
-            		tournament_id: 0,
-            		a_participant_score: userScore,
-            		b_participant_score: opponentScore,
-            	});
+        const isTournamentFinalMatch = Boolean(
+          tournamentState?.active &&
+          tournamentState.matches.length === 1 &&
+          tournamentState.currentMatch === 0
+        );
+        if (!(aiP2 && aiP1) && !isTournamentFinalMatch) {
+          console.log(`current: ${tournamentState?.currentMatch}, is final: ${isTournamentFinalMatch}`);
+          const userScore = aiP1 && !aiP2 ? score2 : score1;
+          const opponentScore = aiP1 && !aiP2 ? score1 : score2;
+          try {
+            await postMatch({
+              tournament_id: 0,
+              a_participant_score: userScore,
+              b_participant_score: opponentScore,
+            });
           } catch (error) {
             console.error("Failed to post match", error);
           }
@@ -135,7 +142,7 @@ export async function renderGame(root: HTMLElement, options: RenderGameOptions =
         setTimeout(() => {
           if (tournament && onGameOver) {
             // tournament: go to next match
-            onGameOver(result);
+            onGameOver(result, tournamentState);
           } else {
             // normal game: show Play Again
             const buttonOverlay = document.createElement("div");
